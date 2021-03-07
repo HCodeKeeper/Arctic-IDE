f1431a98191a0e8b186abbd99421555bc06e712c
HCodeKeeper <58997690+HCodeKeeper@users.noreply.github.com>
2021-03-07 18:09:15 +0200

    Tabs reinforced
    Still with the same issue. Gotta complete this later

--- arctic_ideWIN.py
+++ arctic_ideWIN.py
@@ -74,6 +74,11 @@ class TextField(tk.Text):
         self.yscrollbar.config(command=self.yview)
         self.bind('<Return>', self._InsertTabEvent)
 
+        self.IndLevels = [(0,0),] #To save indentation levels
+        #(0, 0) where [0] => index of ":"(int) in contentList and [1] => quantity of tabs(int)
+
+        self.currentIndLevel = self.IndLevels[-1]
+
     
     def GetContent(self, omit=None):
         content = self.get("1.0", tk.END)
@@ -85,19 +90,44 @@ class TextField(tk.Text):
         return self.contentList
 
     
+    def GetCursorPosition(self):
+        return self.index(tk.INSERT)
+
+
+    def _GetLastIndLevel(self):
+        self.currentIndLevel = self.IndLevels[-1]
+
+
     def _InsertTabEvent(self, event):
         if len(self.content) >= 1:
             if self.content[-1] == ':':
                 self.tabCounter += 1
                 self.isTab = True
+                self.IndLevels.append([self.content[-1], self.IndLevels[-1][1]+1])
 
 
-class MainloopProcessing: #Only for mainloop
+    def ErasingTab(self):
+        if '\n' in reversedContentList and ':' in reversedContentList:
+            if reversedContentList[0] == "\t":
+                for char in reversedContentList[0:reversedContentList.index('\n')]:
+                    if char != '\t':
+                        return False
+                    else:
+                        continue
+                if len(reversedContentList[0:reversedContentList.index('\n')]) < self.currentIndLevel[1] and self.currentIndLevel >= 1:
+                    self.IndLevels.append([len(self.contentList)-1, self.IndLevels[-1][1]-1])
+                        
 
-    def ProcessTabEvents(textFieldObj):
-        if textFieldObj.tabCounter and textFieldObj.isTab:
-            textFieldObj.insert('end', "\t"*textFieldObj.tabCounter)
-        textFieldObj.isTab = False
+
+    def ProcessTabEvents(self):
+        self._GetLastIndLevel()
+        reversedContentList = self.contentList[::-1]
+        if self.tabCounter and self.isTab:
+            self.insert('end', "\t"*self.currentIndLevel[1])
+        else:
+            pass
+        self.isTab = False
+
 
 
 if __name__ == '__main__':
@@ -105,7 +135,7 @@ if __name__ == '__main__':
     while True:
         if program.GetCurrentTab() != None:
             program.currentTab.textField.GetContent()
-            MainloopProcessing.ProcessTabEvents(program.currentTab.textField)
+            program.currentTab.textField.ProcessTabEvents()
         program.update_idletasks()
         program.update()
         time.sleep(0.01)
