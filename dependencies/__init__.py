f0f6b63622dcfabda46aa5a0c47f9f64a84dc906
HCodeKeeper <58997690+HCodeKeeper@users.noreply.github.com>
2021-03-15 18:17:20 +0200

    Menu bar, files creating and closing
    Created the menu bar with blank menus, ability to close and create files, number in default title of each is dynamically changed.
    Objects of the class Tab are now provided with unique names(vars) that are used instead of default titles if the file has been saved before (with name, obviously)

---
+++ dependencies/__init__.py
@@ -0,0 +1,4 @@
+import tkinter as tk
+from tkinter import ttk
+import gui_config as cf 
+import specs
---
+++ gui_config.py
@@ -0,0 +1,15 @@
+class Geometry:
+    width= 1200
+    height = 800
+
+
+class Font:
+    size = 14
+    family = 'Helvetica'
+    fontTuple = (family, size)
+
+
+class TextFieldGeometry:
+    #works if font-size = 14
+    WIDTH = 105
+    HEIGHT = 34
